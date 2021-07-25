d3c6f400283463a3d911cae1990b57ac34c07dff
codelaxx <87918786+codelaxx@users.noreply.github.com>
2021-07-25 18:10:13 +0200

    Update game.js
    Trying to add touch device support

--- game.js
+++ game.js
@@ -38,6 +38,30 @@ function createSquare(idNumber, isMovable) {
         move(event.path[0].id)
     }
 
+    //
+    // Touch device support:::--->
+    //
+    
+    // Touch events
+    item.addEventListener('touchstart', process_touchstart, false);
+    //item.addEventListener('touchmove', process_touchmove, false);
+    //item.addEventListener('touchcancel', process_touchcancel, false);
+    //item.addEventListener('touchend', process_touchend, false);
+
+    // touchstart handler
+    function process_touchstart(ev) {
+    // Use the event's data to call out to the appropriate gesture handlers
+    //switch (ev.touches.length) {
+      //case 1: handle_one_touch(ev); break;
+      //case 2: handle_two_touches(ev); break;
+      //case 3: handle_three_touches(ev); break;
+      //default: gesture_not_supported(ev); break;
+    //}
+      console.log(ev);
+      move("touchstartevent detected, woha!");
+      move(ev);
+    }
+  
     return item;
 }
 
@@ -59,4 +83,3 @@ function debug(output) {
     listItem.innerHTML = output;
     parent.appendChild(listItem);
 }
-
